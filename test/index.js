dfe89c5a01964126f4282b68e1439ec10700bc17
Irakli Gozalishvili <rfobic@gmail.com>
2012-11-01 21:29:50 -0700

    Update to reducers 0.5.x

--- test/index.js
+++ test/index.js
@@ -1,11 +1,7 @@
-/* vim:set ts=2 sw=2 sts=2 expandtab */
-/*jshint asi: true undef: true es5: true node: true browser: true devel: true
-         forin: true latedef: false globalstrict: true*/
-
 "use strict";
 
 var grep = require("../grep-reduce")
-var into = require("reducers/accumulator").into
+var into = require("reducers/into")
 
 var cities = {
   "kiev": 1985,
